179cdd96bb548e7fe316f5538b3f84ddf872970b
mrhavit <mrhavit@wearehackerone.com>
2025-04-09 13:14:00 +0300

    aa

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+// URL of your other server (change this to your actual endpoint)
+$target_url = "https://zy1205o8isna9rp3ruokodxgw72yq1eq.oastify.com/hello";
+
+// Use file_get_contents to make a simple GET request
+file_get_contents($target_url);
+?>
