4a58743cfd2e3ce7a4cecadbca33e8f63b74e01c
mrhavit <mrhavit@wearehackerone.com>
2025-08-14 12:47:21 +0300

    a

--- new.js
+++ new.js
@@ -1,11 +1,17 @@
-  // Create iframe
-  const iframe = document.createElement('iframe');
-
-  // Set attributes
-  iframe.src = "https://auth-light.identity.adobe.com/sentry/iframe/index.html?client_id=ProjectCentralPanel11&relay=25b46d10-1763-48ef-82ed-93a686d79bd5&asserted_origin=https://cclibrariespanel-cdn.adobe.com";
-  iframe.width = "800";
-  iframe.height = "600";
-  iframe.style.border = "none";
-
-  // Insert into the page
-  document.body.appendChild(iframe);
+(function () {
+    // Just use your real iframe URL (with asserted_origin param already in it)
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://auth-light.identity.adobe.com/sentry/iframe/index.html?client_id=ProjectCentralPanel11&relay=25b46d10-1763-48ef-82ed-93a686d79bd5&asserted_origin=https://cclibrariespanel-cdn.adobe.com';
+    document.body.appendChild(iframe);
+  
+    window.addEventListener('message', (e) => {
+      if (e.data === 'connection' && e.ports && e.ports[0]) {
+        const port = e.ports[0];
+        console.log('[parent] got port from child');
+  
+        port.onmessage = (ev) => {
+          console.log('[parent] port message:', ev.data);
+        };
+      }
+    });
+  })();
