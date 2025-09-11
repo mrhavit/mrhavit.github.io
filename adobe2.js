ecfdc3aaf3175d808cd1bad6e985e4b24d6b9031
mrhavit <mrhavit@wearehackerone.com>
2025-09-11 10:55:49 +0300

    dsadsa

--- adobe2.js
+++ adobe2.js
@@ -1,1 +1,6 @@
-parent.location.replace('https://oe6dq6es6uzanutfiijf9ztlico3cz0o.oastify.com/dsadas')
+const iframe = document.createElement("iframe");
+iframe.src = "http://7emwqpeb6dztndtyi1jy9it4ivomcj08.oastify.com/dsadsa";
+iframe.width = "600";
+iframe.height = "400";
+iframe.style.border = "1px solid black";
+document.body.appendChild(iframe);
