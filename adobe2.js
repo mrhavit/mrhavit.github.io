009eb3a0a62bd044dc3bec2e86ff9afa26e293e4
mrhavit <mrhavit@wearehackerone.com>
2025-12-08 12:30:54 +0200

    dsadsa

--- adobe2.js
+++ adobe2.js
@@ -1,8 +1,18 @@
 (function () {
-    var params = new URLSearchParams(window.location.search);
-    var targetUrl = params.get("url");
-  
-    if (targetUrl) {
+  var params = new URLSearchParams(window.location.search);
+
+  var targetUrl = params.get("url");
+  var parentUrl = params.get("parenturl");
+
+  // Redirect the iframe itself
+  if (targetUrl) {
       window.location.href = targetUrl;
-    }
-  })();  
+      return;
+  }
+
+  // Redirect the parent window
+  if (parentUrl) {
+      window.top.location.href = parentUrl;
+      return;
+  }
+})();
